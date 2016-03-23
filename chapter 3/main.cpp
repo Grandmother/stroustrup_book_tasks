e1cbb5bf4fd94f6146d72e3de6c45934bb6c18f5
Roman Kovtyukh <HelloDearGrandma@gmail.com>
2016-03-23 15:59:50 +0300

    Chapter 3. Hello %username%. I know your age./a.out

--- chapter 3/main.cpp
+++ chapter 3/main.cpp
@@ -3,10 +3,12 @@
 int main()
 {
     std::string first_name;
+    unsigned int age = 0;
 
-    std::cout << "Enter your first name (and press 'Enter'): ";
+    std::cout << "Enter your first name and your age (and press 'Enter'): ";
     std::cin >> first_name;
-    std::cout << "Hello, " << first_name << "!\n";
+    std::cin >> age;
+    std::cout << "Hello, " << first_name << "!\nYou are " << age << " years old and you are dying.\n";
 
     return 0;
 }
