e1b6fbdd5cb123ebdaa05fea3de5bc9cbf6e0b15
Roman Kovtyukh <HelloDearGrandma@gmail.com>
2016-03-23 16:10:39 +0300

    Chapter 3. + age in months.

--- chapter 3/main.cpp
+++ chapter 3/main.cpp
@@ -2,13 +2,12 @@
 
 int main()
 {
-    std::string first_name;
-    unsigned int age = 0;
+    std::string first_name, second_name;
+    double age = -1;
 
-    std::cout << "Enter your first name and your age (and press 'Enter'): ";
-    std::cin >> first_name;
-    std::cin >> age;
-    std::cout << "Hello, " << first_name << "!\nYou are " << age << " years old and you are dying.\n";
+    std::cout << "Enter your first and second name and your age (and press 'Enter'): ";
+    std::cin >> first_name >> second_name >> age;
+    std::cout << "Hello, " << first_name << " " << second_name << "!\nYou are " << static_cast<int>(age*12) << " month old and you are dying.\n";
 
     return 0;
 }
