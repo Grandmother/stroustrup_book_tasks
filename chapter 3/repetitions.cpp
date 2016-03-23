386276da56f9c4c8dabcb15867e847617436e4f6
Roman Kovtyukh <HelloDearGrandma@gmail.com>
2016-03-23 19:36:37 +0300

    Chapter 3. Number of repeated word.

--- chapter 3/repetitions.cpp
+++ chapter 3/repetitions.cpp
@@ -3,14 +3,16 @@
 
 int main()
 {
+    int number_of_words = 0;
     std::string previous = " ";
-
     std::string current;
+
     while (std::cin >> current)
     {
+        ++number_of_words;
         if (previous == current)
         {
-            std::cout << current << " is repeated.\n";
+            std::cout << number_of_words << " word (" << current << ") is repeated.\n";
         }
         previous = current;
     }
