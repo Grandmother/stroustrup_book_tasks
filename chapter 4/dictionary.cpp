af8e02ed1cd337c94e36828e33c252fb614cdd08
Roman Kovtyukh <HelloDearGrandma@gmail.com>
2016-04-12 16:56:19 +0300

    Chapter 4. Simple dictionary.

---
+++ chapter 4/dictionary.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+#define IOERR 74
+
+int main()
+{
+    vector<string> dictionary;
+
+    cout << "Enter some words to dictionary:\n";
+    for ( string temp ; cin >> temp; )
+    {
+        dictionary.push_back(temp);
+    }
+
+    sort(dictionary.begin(), dictionary.end());
+    cout << "\n\nWords in dictionary:\n";
+    for ( int i = 0; i < dictionary.size(); ++i )
+    {
+        if ( i == 0 || dictionary[i-1] != dictionary[i] )
+        {
+            cout << dictionary[i] << '\n';
+        }
+    }
+    return 0;
+}
