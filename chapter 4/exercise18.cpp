0ab775180c56069271751dbc9951478de832fc17
Roman Kovtyukh <HelloDearGrandma@gmail.com>
2016-04-21 20:18:44 +0300

    Chapter 4. Exercise18.

--- chapter 4/exercise18.cpp
+++ chapter 4/exercise18.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 int main()
 {
     double a, b, c;
@@ -12,14 +14,21 @@ int main()
     double x1, x2;
     if ( D > 0 )
     {
-        x1 = 
+        x1 = (-b + sqrt(D)) / 2*a;
+        x2 = (-b - sqrt(D)) / 2*a;
+
+        cout << "x1 = " << x1 << '\n'
+            << "x2 = " << x2 << '\n';
     }
     else if ( D == 0 )
     {
-
+        x1 = -b / 2*a;
+        cout << "x = " << x1 << '\n';
     }
     else
     {
         cout << "This equation has no real root.\n";
     }
+
+    return 0;
 }
